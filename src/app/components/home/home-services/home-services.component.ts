8563055a4b727c33d532de7720b467255a9cc44e
Olivier Riccini <olivierriccini@olivier.local>
2020-11-05 22:18:29 -0500

    change services

--- src/app/components/home/home-services/home-services.component.ts
+++ src/app/components/home/home-services/home-services.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-services.component.scss']
 })
 export class HomeServicesComponent implements OnInit {
-  public services = ['dapp', 'smartcontracts', 'crytpowallets', 'tokens', 'consulting', 'integration', 'training'];
+  public services = ['dapp', 'smartContractDev', 'smartContractAudit', 'digitalAssets', 'cryptoWallet'];
 
   constructor() { }
 
