12b9fd4ac31afb52cbe4857af050897eb785bb39
Olivier Riccini <olivierriccini@olivier.local>
2020-12-05 14:18:37 -0500

    add image preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import AOS from 'aos';
 
+import * as ImagePreloader from 'image-preloader';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +14,7 @@ export class AppComponent implements AfterViewInit {
   title = 'Blockbrainers';
   public isBrowser = false;
   public topened = false;
+  private preloader = new ImagePreloader();
 
   constructor(@Inject(PLATFORM_ID) private platformId: any, private router: Router) {
     this.isBrowser = isPlatformBrowser(this.platformId);
@@ -20,7 +23,10 @@ export class AppComponent implements AfterViewInit {
         disable: 'mobile'
       });
     }
-
+    this.preloader.preload('./assets/images/about/team.jpg')
+      .then((status) => {
+          console.log('all done!', status);
+      });
 
   }
 
