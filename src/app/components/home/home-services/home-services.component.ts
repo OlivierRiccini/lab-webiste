1ecb339081f510e9007c5de246123dc8d7d3f041
Olivier Riccini <olivierriccini@olivier.local>
2020-12-05 15:57:36 -0500

    mispellings

--- src/app/components/home/home-services/home-services.component.ts
+++ src/app/components/home/home-services/home-services.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-services.component.scss']
 })
 export class HomeServicesComponent implements OnInit {
-  public services = ['dapp', 'smartContractDev', 'smartContractAudit', 'digitalAssets', 'cryptoWallet', 'integration', 'training'];
+  public services = ['dapp', 'smartContractsDev', 'smartContractsAudit', 'digitalAssets', 'cryptoWallet', 'integration', 'training'];
 
   constructor() { }
 
